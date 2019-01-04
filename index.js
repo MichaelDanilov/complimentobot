b15bdcf869215576ce84a0972ff6342ff71ede96
Michael Danilov <michael@danilov.me>
2019-01-04 11:40:19 +0600

    Fix intentation

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const compliments = require('./compliments.json');
 const sampleSize = require('lodash.samplesize');
 
-const token = process.env.NODE_ENV;
+const token = process.env.TG_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, msg => {
-	const message = [
-		'/random - случайный комплимент',
-		'/random Х - Х случайных комплиментов',
-		'/last - последний комплимент',
-		'/last X - X последних комплиментов',
-	].join('\n');
-	bot.sendMessage(msg.from.id, message);
+  const message = [
+    '/random - случайный комплимент',
+    '/random Х - Х случайных комплиментов',
+    '/last - последний комплимент',
+    '/last X - X последних комплиментов',
+  ].join('\n');
+  bot.sendMessage(msg.from.id, message);
 });
 
 bot.onText(/\/random( ([0-9]+))*/, (msg, match) => {
@@ -25,40 +25,40 @@ bot.onText(/\/last( ([0-9]+))*/, (msg, match) => {
 });
 
 bot.on('inline_query', msg => {
-	bot.answerInlineQuery(msg.id, toInlineQuery(getRandom(5)));
+  bot.answerInlineQuery(msg.id, toInlineQuery(getRandom(5)));
 })
 
 function getRandom(n = 1) {
-	return sampleSize(compliments, n);
+  return sampleSize(compliments, n);
 }
 
 function getLast(n = 1) {
-	return compliments.slice().reverse().slice(0, n);
+  return compliments.slice().reverse().slice(0, n);
 }
 
 function concatMessage(list = []) {
-	let message = '';
-	list.map(item => {
-		message = `${message}\n${item.text}`;
-	});
-	return message;
+  let message = '';
+  list.map(item => {
+    message = `${message}\n${item.text}`;
+  });
+  return message;
 }
 
 function toInlineQuery(list = []) {
-	const articles = [];
-	list.map((item, index) => {
-		articles.push({
-			type: 'article',
-			id: `${Math.random()}`,
-			title: item.text,
-			cache_time: 60,
-			input_message_content: {
-				message_text: item.text
-			},
-			thumb_url: `https://s.dnlv.co/bots/complimentobot/flower${(index + 1)}.jpg`,
-			thumb_width: 200,
-			thumb_height: 200,
-		});
-	});
-	return articles;
+  const articles = [];
+  list.map((item, index) => {
+    articles.push({
+      type: 'article',
+      id: `${Math.random()}`,
+      title: item.text,
+      cache_time: 60,
+      input_message_content: {
+        message_text: item.text
+      },
+      thumb_url: `https://s.dnlv.co/bots/complimentobot/flower${(index + 1)}.jpg`,
+      thumb_width: 200,
+      thumb_height: 200,
+    });
+  });
+  return articles;
 }
